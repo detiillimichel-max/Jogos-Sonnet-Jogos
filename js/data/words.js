2a1c4538e0fe1ba2b27386c0fe755c281c332e97
Michel Detiilli <detiillimichel@gmail.com>
2026-06-02 19:07:56 -0300

    Refactor WordData structure and update words

--- js/data/words.js
+++ js/data/words.js
@@ -5,124 +5,6 @@
 const WordData = {
   themes: [
     {
-      id:    'animais',
-      name:  'Animais',
-      icon:  '🐾',
-      color: '#39f587',
-      levels: [
-        { words: ['GATO','LEAO','URSO','LOBO','PATO'],                          size: 8,  time: 90,  maxErrors: 5 },
-        { words: ['COBRA','TIGRE','GIRAFA','MACACO','ONCA'],                     size: 10, time: 110, maxErrors: 4 },
-        { words: ['CACHORRO','ELEFANTE','PAPAGAIO','JACARE','TARTARUGA'],        size: 12, time: 150, maxErrors: 3 },
-      ]
-    },
-    {
-      id:    'frutas',
-      name:  'Frutas',
-      icon:  '🍎',
-      color: '#ff4d6d',
-      levels: [
-        { words: ['MACA','PERA','UVA','KIWI','FIGO'],                           size: 8,  time: 90,  maxErrors: 5 },
-        { words: ['BANANA','MANGA','MELAO','MORANGO','LIMAO'],                   size: 10, time: 110, maxErrors: 4 },
-        { words: ['ABACAXI','MARACUJA','FRAMBOESA','GOIABA','LARANJA'],          size: 12, time: 150, maxErrors: 3 },
-      ]
-    },
-    {
-      id:    'esportes',
-      name:  'Esportes',
-      icon:  '⚽',
-      color: '#ffe033',
-      levels: [
-        { words: ['SURF','BOXE','POLO','GOLF','LUTA'],                           size: 8,  time: 90,  maxErrors: 5 },
-        { words: ['TENIS','VOLEI','FUTEBOL','NATACAO','CICLISMO'],               size: 10, time: 110, maxErrors: 4 },
-        { words: ['BASQUETE','GINASTICA','ATLETISMO','HANDEBOL','BADMINTON'],    size: 12, time: 150, maxErrors: 3 },
-      ]
-    },
-    {
-      id:    'paises',
-      name:  'Países',
-      icon:  '🌍',
-      color: '#00e5ff',
-      levels: [
-        { words: ['CHILE','CHINA','CUBA','PERU','IRAQ'],                          size: 8,  time: 90,  maxErrors: 5 },
-        { words: ['BRASIL','JAPAO','ITALIA','RUSSIA','CANADA'],                   size: 10, time: 110, maxErrors: 4 },
-        { words: ['AUSTRALIA','ALEMANHA','PORTUGAL','ARGENTINA','COLOMBIA'],     size: 12, time: 150, maxErrors: 3 },
-      ]
-    },
-    {
-      id:    'cores',
-      name:  'Cores',
-      icon:  '🎨',
-      color: '#b44dff',
-      levels: [
-        { words: ['AZUL','ROSA','ROXO','PRETO','VERDE'],                         size: 8,  time: 90,  maxErrors: 5 },
-        { words: ['LARANJA','AMARELO','VERMELHO','BRANCO','CINZA'],              size: 10, time: 110, maxErrors: 4 },
-        { words: ['TURQUESA','MAGENTA','DOURADO','PRATA','MARROM'],              size: 12, time: 150, maxErrors: 3 },
-      ]
-    },
-  ]
-};
-const WordData = {
-  themes: [
-    {
-      id:    'animais',
-      name:  'Animais',
-      icon:  '🐾',
-      color: '#39f587',
-      levels: [
-        { words: ['GATO','LEAO','URSO','LOBO','PATO'], size: 8, time: 90, maxErrors: 5 },
-        { words: ['COBRA','TIGRE','GIRAFA','MACACO','ONCA'], size: 10, time: 110, maxErrors: 4 },
-        { words: ['CACHORRO','ELEFANTE','PAPAGAIO','JACARE','TARTARUGA'], size: 12, time: 150, maxErrors: 3 },
-      ]
-    },
-    {
-      id:    'frutas',
-      name:  'Frutas',
-      icon:  '🍎',
-      color: '#ff4d6d',
-      levels: [
-        { words: ['MACA','PERA','UVA','KIWI','FIGO'], size: 8, time: 90, maxErrors: 5 },
-        { words: ['BANANA','MANGA','MELAO','MORANGO','LIMAO'], size: 10, time: 110, maxErrors: 4 },
-        { words: ['ABACAXI','MARACUJA','FRAMBOESA','GOIABA','LARANJA'], size: 12, time: 150, maxErrors: 3 },
-      ]
-    },
-    {
-      id:    'esportes',
-      name:  'Esportes',
-      icon:  '⚽',
-      color: '#ffe033',
-      levels: [
-        { words: ['SURF','BOXE','POLO','GOLF','LUTA'], size: 8, time: 90, maxErrors: 5 },
-        { words: ['TENIS','VOLEI','FUTEBOL','NATACAO','CICLISMO'], size: 10, time: 110, maxErrors: 4 },
-        { words: ['BASQUETE','GINASTICA','ATLETISMO','HANDEBOL','BADMINTON'], size: 12, time: 150, maxErrors: 3 },
-      ]
-    },
-    {
-      id:    'paises',
-      name:  'Países',
-      icon:  '🌍',
-      color: '#00e5ff',
-      levels: [
-        { words: ['CHILE','CHINA','CUBA','PERU','IRAQ'], size: 8, time: 90, maxErrors: 5 },
-        { words: ['BRASIL','JAPAO','ITALIA','RUSSIA','CANADA'], size: 10, time: 110, maxErrors: 4 },
-        { words: ['AUSTRALIA','ALEMANHA','PORTUGAL','ARGENTINA','COLOMBIA'], size: 12, time: 150, maxErrors: 3 },
-      ]
-    },
-    {
-      id:    'cores',
-      name:  'Cores',
-      icon:  '🎨',
-      color: '#b44dff',
-      levels: [
-        { words: ['AZUL','ROSA','ROXO','PRETO','VERDE'], size: 8, time: 90, maxErrors: 5 },
-        { words: ['LARANJA','AMARELO','VERMELHO','BRANCO','CINZA'], size: 10, time: 110, maxErrors: 4 },
-        { words: ['TURQUESA','MAGENTA','DOURADO','PRATA','MARROM'], size: 12, time: 150, maxErrors: 3 },
-      ]
-    },
-  ]
-};
-const WordData = {
-  themes: [
-    {
       id: 'animais',
       name: 'Animais',
       icon: '🐾',
@@ -151,7 +33,7 @@ const WordData = {
         { words: ['JABUTICABA','CARAMBOLA','GRAVIOLA','TAMARINDO','CUPUACU'], size: 12, time: 135, maxErrors: 4 },
         { words: ['MELANCIA','PESSEGO','DAMASCO','MAMAO','COCO'], size: 13, time: 150, maxErrors: 3 },
         { words: ['ABACATE','AMORA','PITAYA','JACA','MANGABA'], size: 14, time: 165, maxErrors: 3 },
-        { words: ['FRUTA PAO','JENIPAPO','CAMBUCI','BACURI','UMBU'], size: 15, time: 180, maxErrors: 3 },
+        { words: ['FRUTAPAO','JENIPAPO','CAMBUCI','BACURI','UMBU'], size: 15, time: 180, maxErrors: 3 },
       ]
     },
     {
@@ -166,7 +48,7 @@ const WordData = {
         { words: ['ESGRIMA','JUDO','KARATE','RUGBY','SKATE'], size: 11, time: 120, maxErrors: 4 },
         { words: ['MARATONA','TRIATLO','CANOAGEM','ESCALADA','PATINACAO'], size: 12, time: 135, maxErrors: 4 },
         { words: ['BICICROSS','PARAQUEDISMO','WINDSURF','KITESURF','SNOWBOARD'], size: 13, time: 150, maxErrors: 3 },
-        { words: ['FUTSAL','BEISEBOL','CRIQUETE','POLO AQUATICO','TIRO COM ARCO'], size: 14, time: 165, maxErrors: 3 },
+        { words: ['FUTSAL','BEISEBOL','CRIQUETE','POLOAQUATICO','TIROCOMARCO'], size: 14, time: 165, maxErrors: 3 },
         { words: ['CURLING','BIATLO','ESQUI','HOCKEY','LEVANTAMENTO'], size: 15, time: 180, maxErrors: 3 },
       ]
     },
